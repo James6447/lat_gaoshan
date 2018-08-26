0de72e660b45503fbf552d02637b7da6cb79ac58
zhijian090195 <zhijian090195@hotmail.com>
2018-08-26 13:43:24 +0800

    chinese

---
+++ chineese/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>LAUSINCERE-HOME</title>
+  <meta charset="utf-8">
+  <noscript><iframe src=*.html></iframe></noscript>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <META NAME="Author" CONTENT="LauSincere Autowagon Services - 高山輪胎有限公司">
+  <META HTTP-EQUIV="Keywords" CONTENT="tyres maintenance,LauSincere Autowagon Services, Services,engine oil, services,Trading Of Tyres And Batteries Of Motor Vehicles,johor bahru,高山輪胎有限公司">
+  <META HTTP-EQUIV="Description" CONTENT="http://www.lausincere.com, Automotive Repair Shop, tyres, engine oil, vihicle services">
+
+  <link rel="icon" type="image/png" href="img/logo-1.png" />
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+  <link href="css/header.css" rel="stylesheet">
+  <link href="css/footer.css" rel="stylesheet">
+  <script src="js/jquery.js"></script>
+  <script src="js/product.js"></script>
+  <script src="js/header.js"></script>
+  <script src="js/win.js"></script>
+</head>
+<body onscroll="backTop()">
+
+<?php include('header.html') ?>
+
+<!-- picture holder container main.js mian.css-->
+<div id="search-bar" class="row search justify-content-center">
+  <div id="title">
+    <h5>Find Your Type Here</h5>
+    <img src="img/icon-search.png">
+  </div>
+
+  <div id="type" class="type-wrap">
+      <div class="vihicle">
+
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              By Vehicle
+            </button>
+             <div id="vihicle-wrapper" class="dropdown-menu dropdown-menu-right">
+              <div class="dropdown-item item-wrapper">
+                <!-- <div class="row col-md-12">
+                    <p class="col-md-6">By Car</p>
+                    <p class="col-md-6">By Year</p>
+                </div> -->
+                <div class="row col-md-12">
+                 <form action="product.php" method="post">
+                     <div class="col-md-6 col-xs-12 ">
+                         <p class="col-md-6">CAR BRANDS</p>
+                          <select id="typeCar" class="maintain" name="carType"  onchange="TypeFunc();">
+                            <option value="1">Audi</option>
+                            <option value="2">BMW</option>
+                            <option value="3">Ford</option>
+                            <option value="4">Honda</option>
+                          </select>
+                      </div>
+                      <div class="col-md-6 col-xs-12">
+                          <p class="col-md-6">CAR MODEL</p>
+                          <select id="modelCar" class="maintain" name="carModel">
+
+                          </select>
+                      </div>
+                </div>
+                <div class="row submit d-flex justify-content-center">
+                    <button type="submit">Submit</button>
+                </div>
+               </form>
+              </div>
+            </div>
+
+        </div>
+      </div>
+      <div class="dash">
+        <h5>|</h5>
+      </div>
+      <div class="size">
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            By Size
+          </button>
+          <div id="size-wrapper" class="dropdown-menu">
+              <div class="dropdown-item item-wrapper">
+                <div class="row col-md-12">
+                     <div class="col-md-6 col-12">
+                         <form action="product.php" method="post">
+                             <div class="col-md-6">
+                                 <p>By Brand</p>
+                                  <select id="typeModel" class="maintain" name="rimType">
+                                      <option value="1">Audi</option>
+                                      <option value="2">BMW</option>
+                                      <option value="3">Ford</option>
+                                      <option value="4">Honda</option>
+                                  </select>
+                              </div>
+                              <div class="col-md-6">
+                                 <p>By Inch</p>
+                                  <select id="inch" class="maintain rol-md-5 rol-xs-12" name="size">
+                                      <!-- <option value="13">13</option>
+                                      <option value="14">14</option>
+                                      <option value="15">15</option>
+                                      <option value="16">16</option> -->
+                                      <option value="17">17</option>
+                                      <option value="18">18</option>
+                                      <option value="19">19</option>
+                                      <option value="20">20</option>
+                                  </select>
+                              </div>
+                              <div class="col-md-6 submit">
+                                 <button type="submit">Submit</button>
+                              </div>
+                            </form>
+                             </div>
+                            <div class="col-md-6 col-12" >
+                                  <img src="img/icon-tyre.png">
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+  <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="img/background-3.jpg" alt="First slide">
+        <div class="carousel-caption d-none d-md-block">
+          <!-- <h1>Product</h1>
+          <img src= -->
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="img/background-1.jpg" alt="Second slide">
+      </div>
+
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+
+
+
+<!-- linked main.css-->
+<a id="fb" class="link-facebook animated infinite bounce" href="https://web.facebook.com/Lausincereautowagon/" target="_blank" onmousemove="stop()" onmouseout="start()"><img src="img/button fb.png"></a>
+<span class="link-menu" id="head" ><img src="img/button up.png"></span>
+
+<!-- Footer -->
+<?php
+include('footer.html');
+?>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
---
+++ chineese/services.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>SERVICES</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <noscript><iframe src=*.html></iframe></noscript>
+  <link rel="icon" type="image/png" href="img/logo-1.png" />
+  <link src="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/footer.css" rel="stylesheet">
+  <link href="css/head.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/brands.css" integrity="sha384-VGCZwiSnlHXYDojsRqeMn3IVvdzTx5JEuHgqZ3bYLCLUBV8rvihHApoA1Aso2TZA" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
+  <link href="css/service.css" rel="stylesheet">
+  <link href="css/header.css" rel="stylesheet">
+  <script src="js/jquery.js"></script>
+  <script src="js/header.js"></script>
+  <script src="js/win.js"></script>
+  <script src="js/main.js"></script>
+</head>
+<body  onscroll="backTop()">
+  <?php
+  include('header.html');
+  ?>
+<h1 class="breadcrumb-title"><span class="brand-title"><img src='img/Service.png'></span>&nbsp;&nbsp;&nbsp;Services</h1>
+<div id="containersevices">
+    <div class="videoyou">
+      <iframe class="youtubeclass" width="100%" height="100%"
+        src="https://youtube.com/embed/uTqkQutK_nc">
+      </iframe>
+    </div>
+</div>
+
+
+
+<div class="containeralbum">
+  <div class="row grid ">
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 card grid-item">
+      <div class="card box-shadow">
+        <img class="card-img-top" style="height: 100%; width: 100%; display: block;"
+          src="img/Tire Maintenance.jpg">
+        <div class="card-body ">
+          <h4 class ="card-title">轮胎维护</h4>
+          <p class="card-text">有的时侯，轮胎会出现内部空气泄漏导致轮胎爆裂，必须避免这种情况，以便确保驾驶者不会有任何顾虑的行驶在路上。</p>
+          <!-- Button trigger modal -->
+          <button type="button" class="btn align-self-end effect " data-toggle="modal" data-target="#myModal0">
+            <div class="txtbtn">了解更多</div> <i class="fa fa-arrow-right"></i>
+          </button>
+              <!-- Modal -->
+          <div id="myModal0" class="modal fade" data-easein="bounceIn"  tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+           <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">轮胎维护</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="videoyou">
+              <iframe class="youtubeclass" width="100%" height="100%"src="https://youtube.com/embed/uTqkQutK_nc">
+              </iframe>
+              </div>
+              <div class="modal-body"><br>
+              汽车的轮胎维护基于几个因素。轮胎更换的主要原因是与路面移动接触引起的摩擦，导致轮胎外周上的胎面最终磨损。当胎面深度变得太浅（小于0.125英寸/3.2毫米）时，轮胎磨损并应更换。通常可以在汽车的整个使用寿命期间使用相同的类型的轮胎。<br><br>
+              <span class="message-notice">* 如果以下几个步骤困扰您，请联系我们以获得更专业的服务。<br></span>
+              <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+              </div>
+
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 card grid-item">
+      <div class="card box-shadow">
+        <img class="card-img-top" style="height: 100%; width: 100%; display: block;"
+          src="img/Battery Service.jpg">
+        <div class="card-body ">
+          <h4 class ="card-title">蓄电池服务</h4>
+          <p class="card-text"> 汽车在平时状态下电池并不会出现太大问题，当经过长时间的行驶电池的状态电量可能会因此耗光，所以定期维护电池是每个驾驶所需要知道的。</p>
+          <!-- Button trigger modal -->
+          <button type="button" class="btn align-self-end effect " data-toggle="modal" data-target="#myModal1">
+            <div class="txtbtn">了解更多</div> <i class="fa fa-arrow-right"></i>
+          </button>
+              <!-- Modal -->
+          <div id="myModal1" class="modal fade" data-easein="bounceIn"  tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+           <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">蓄电池服务</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <img class="modal-img-top" style="height: 100%; width: 100%; display: block;"
+              src="img/Battery Service.jpg">
+              <div class="modal-body"><br>
+              汽车在平时状态下电池并不会出现太大问题，当经过长时间的行驶电池的状态电量可能会因此耗光，所以定期维护电池是每个驾驶所需要知道的。<br>
+              以下是轻松更换电池的几个步骤：<br><br>
+              步骤1: 清洁电缆<br>
+              步骤2: 检查电解液的水平<br>
+              步骤3: 检查电池的状况和电量<br>
+              步骤4: 要先放入新电池，请先取下电缆<br>
+              步骤5: 更换电池<br>
+              步骤6: 重新安装夹具和电缆<br><br>
+              <span class="message-notice">* 如果以下几个步骤困扰您，请联系我们以获得更专业的服务。<br></span></div>
+              <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 card grid-item">
+      <div class="card box-shadow">
+        <img class="card-img-top" style="height: 100%; width: 100%; display: block;"
+          src="img/Service.jpg">
+        <div class="card-body ">
+          <h4 class ="card-title">服务</h4>
+          <p class="card-text">车辆服务在车辆行驶一定距离之后设定的时间然后执行的一系列维护程序。维修间隔由车辆制造商在维修计划中指定，并且一些现代车辆在仪表板上以电子方式显示下一维修的到期日。</p>
+          <!-- Button trigger modal -->
+          <button type="button" class="btn align-self-end effect " data-toggle="modal" data-target="#myModal2">
+            <div class="txtbtn">了解更多</div> <i class="fa fa-arrow-right"></i>
+          </button>
+              <!-- Modal -->
+          <div id="myModal2" class="modal fade" data-easein="bounceIn"  tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+           <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">维护</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <img class="modal-img-top" style="height: 100%; width: 100%; display: block;"
+              src="img/Service.jpg">
+              <div class="modal-body"><br>
+              车辆维护在车辆行驶一定距离之后设定的时间然后执行的一系列维护程序。维修间隔由车辆制造商在维修计划中指定，并且一些现代车辆在仪表板上以电子方式显示下一维修的到期日。<br>
+              通常在汽车服务期间执行的维护任务包括：<br><br>
+              <li>更换发动机机油</li>
+              <li>更换机油滤清器</li>
+              <li>更换空气过滤器</li>
+              <li>更换燃油滤清器</li>
+              <li>更换机舱过滤器</li>
+              <li>更换火花塞</li>
+              <li>调整引擎</li>
+              <li>检查液位并重新加注制动液/离合器液</li>
+              <li>检查制动蹄片/衬里，制动盘/鼓，如果磨损则更换。</li>
+              <li>检查水平并重新填充动力转向液</li>
+              <li>检查水平并重新填充自动/手动变速箱油</li>
+              <li>润滑和润滑组件</li>
+              <li>如果需要，检查并更换同步带或正时链</li>
+              <li>检查轮胎的状况</li>
+              <li>检查所有灯，刮水器等的正确操作</li>
+              <li>检查ECU中的任何错误代码并采取纠正措施。</li>
+              <li>清洗车辆并清洁内部。</li>
+              <li>使用扫描工具读取故障代码。</li>
+              <br><br>
+              <span class="message-notice">* 如果以下几个步骤困扰您，请联系我们以获得更专业的服务。<br></span></div>
+              <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 card grid-item">
+      <div class="card box-shadow">
+        <img class="card-img-top" style="height: 100%; width: 100%; display: block;"
+          src="img/point.jpg">
+        <div class="card-body ">
+          <h4 class ="card-title">如何自己维护车子</h4>
+          <p class="card-text">拥有汽车有非常多好处，如果您希望长时间享受汽车，必须维护您的汽车。频繁的汽车保养延长了车辆的使用寿命，使其保持平稳运行，不存在任何安全问题。通过定期维护使您的车辆保持在最佳状态，您还可以保留最佳价值，以便在您想要出售汽车时使用。因此，我们编制了一份全面的维护技巧列表，以确保您的汽车的运行性能和美观完整性</p>          
+          <!-- Button trigger modal -->
+          <button type="button" class="btn align-self-end effect " data-toggle="modal" data-target="#myModal3">
+            <div class="txtbtn">了解更多</div> <i class="fa fa-arrow-right"></i>
+          </button>
+              <!-- Modal -->
+          <div id="myModal3" class="modal fade" data-easein="bounceIn"  tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+           <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Maintain Your Own Car</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <img class="modal-img-top" style="height: 100%; width: 100%; display: block;"
+              src="img/point1.jpg">
+              <div class="modal-body"><br>
+              拥有汽车有非常多好处，如果您希望长时间享受汽车，必须维护您的汽车。频繁的汽车保养延长了车辆的使用寿命，使其保持平稳运行，不存在任何安全问题。通过定期维护使您的车辆保持在最佳状态，您还可以保留最佳价值，以便在您想要出售汽车时使用。因此，我们编制了一份全面的维护技巧列表，以确保您的汽车的运行性能和美观完整性<br><br>
+              您要做的事情去维护自己的汽车 
+              <br>
+              <li>检查轮胎</li>
+              <li>换油</li>
+              <li>清洁并固定挡风玻璃，后视镜和灯</li>
+              <li>更换空气过滤器</li>
+              <li>保持室内清洁</li>
+              <li>检查液体</li>
+              <li>控制排放</li>
+              <li>检查电池/li>
+              <br>
+              <span class="message-notice">* 如果以下几个步骤困扰您，请联系我们以获得更专业的服务。<br></span></div>
+              <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 card grid-item">
+      <div class="card box-shadow">
+        <img class="card-img-top" style="height: 100%; width: 100%; display: block;"
+          src="img/checktyre.jpg">
+        <div class="card-body ">
+          <h4 class ="card-title">检查轮胎<</h4>
+          <p class="card-text">您应定期检查轮胎压力，因为它不仅会影响汽车的舒适性，安全性和操控性，还会影响燃油效率。轮胎的不充分膨胀会妨碍车轮正确停止的能力并增加轮胎爆胎的可能性。</p>
+          <!-- Button trigger modal -->
+          <button type="button" class="btn align-self-end effect " data-toggle="modal" data-target="#myModal4">
+            <div class="txtbtn">了解更多</div> <i class="fa fa-arrow-right"></i>
+          </button>
+              <!-- Modal -->
+          <div id="myModal4" class="modal fade" data-easein="bounceIn"  tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+           <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">检查轮胎</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <img class="modal-img-top" style="height: 100%; width: 100%; display: block;"
+              src="img/checktyre1.jpg">
+              <div class="modal-body"><br>
+              您应定期检查轮胎压力，因为它不仅会影响汽车的舒适性，安全性和操控性，还会影响燃油效率。轮胎的不充分膨胀会妨碍车轮正确停止的能力并增加轮胎爆胎的可能性。<br><br>
+              您可以每周检查轮胎压力，但专家建议您每次填充气体时都要这样做。频繁的监控可以让您立即发现任何问题，因为轮胎压力会因温度波动和里程积累而不断变化。制造商的手册规定了推荐的轮胎压力。<br><br>
+              此外，您应该每5,000至10,000英里旋转轮胎。定期旋转车轮确保所有轮胎具有相同的磨损模式，从而延长车轮的使用寿命。<br><br>
+              最后，当轮胎磨损超过合理限度时更换轮胎，以避免发生任何事故<br><br>
+              <span class="message-notice">* 如果以下几个步骤困扰您，请联系我们以获得更专业的服务。<br></span></div>
+              <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 card grid-item">
+      <div class="card box-shadow">
+        <img class="card-img-top" style="height: 100%; width: 100%; display: block;"
+          src="img/wind.jpg">
+        <div class="card-body ">
+          <h4 class ="card-title">清洁并修复</h4>
+          <p class="card-text">清洁并固定挡风玻璃，后视镜和灯具，脏的挡风玻璃是一种安全隐患，因为它可能会在驾驶时遮挡您的视线，并且看起来也很不舒服。因此，请定期用海绵和清洁液擦拭挡风玻璃，具体取决于您当天登记的里程数，每日路线的数量以及停放车辆的位置。</p>
+          <!-- Button trigger modal -->
+          <button type="button" class="btn align-self-end effect " data-toggle="modal" data-target="#myModal5">
+            <div class="txtbtn">了解更多</div> <i class="fa fa-arrow-right"></i>
+          </button>
+              <!-- Modal -->
+          <div id="myModal5" class="modal fade" data-easein="bounceIn"  tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+           <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Clean and fix</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <img class="modal-img-top" style="height: 100%; width: 100%; display: block;"
+              src="img/wind1.jpg">
+              <div class="modal-body"><br>
+              
+              清洁并固定挡风玻璃，后视镜和灯具，脏的挡风玻璃是一种安全隐患，因为它可能会在驾驶时遮挡您的视线，并且看起来也很不舒服。因此，请定期用海绵和清洁液擦拭挡风玻璃，具体取决于您当天登记的里程数，每日路线的数量以及停放车辆的位置。<br><br>
+              挡风玻璃刮水器每年需要更换一次，因为它们在使用时会磨损。在雨季，撕裂的雨刷无法充分发挥作用，并且可能构成安全威胁，特别是如果您在倾盆大雨中行驶。您可以根据其状况仅更换雨刷片或整个组件。<br><br>
+              此外，您需要保持前视镜和后视镜的清洁，因为它们提供了安全驾驶所必需的全方位视野。修理任何在事故中损坏的镜子，立即给他们定期擦拭，以防止视线模糊。<br><br>
+              最后，您车上的灯具有重要的工作，在定期维护时不应忽视。为了确保它们都处于完美的工作状态，请在打开每个灯时让朋友绕过汽车。立即更换任何破损或有故障的灯泡，尤其是前灯。您可以通过几个简单的步骤自己完成。<br><br>
+              <span class="message-notice">*  如果以下几个步骤困扰您，请联系我们以获得更专业的服务。<br></span></div>
+              
+              <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- linked main.css-->
+<a id="fb" class="link-facebook animated infinite bounce" href="https://web.facebook.com/Lausincereautowagon/" target="_blank" onmousemove="stop()" onmouseout="start()"><img src="img/button fb.png"></a>
+<span class="link-menu" id="head" ><img src="img/button up.png"></span>
+
+<!-- Footer -->
+<?php
+include('footer.html');
+?>
+
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="https://unpkg.com/masonry-layout@4.2.1/dist/masonry.pkgd.min.js"></script>
+<script src="js/main.js"></script>
+<script src="js/services.js"></script>
+</body>
+
+
+</html>
