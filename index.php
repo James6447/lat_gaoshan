e77d6188888f647587380ac795fcf1bfd95066f6
James <jamesvyean38@gmail.com>
2018-09-14 12:31:12 +0700

    #debug revert to commit 2bc70532ec6495428824aba693823d907efa45d9

--- index.php
+++ index.php
@@ -39,6 +39,38 @@ $sort = $sort_handle->sortKeywords($arr, 2);
 <?php include('header.html') ?>
 
 <!-- picture holder container main.js mian.css-->
+<div id="search-bar" class="row search justify-content-center container-fluid ">
+
+            <button type="button" class="btn btn-secondary" data-toggle="modal" data-target=".vehicle">Find type at here &nbsp&nbsp<img src="img/icon-search.png"></button>
+              <div class="modal fade vehicle" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                  <div class="modal-content">
+                    <div class ="container-fluid col-12 pt-4 pb-4 tabcolor">
+                      <ul class="nav nav-tabs " id="myTab" role="tablist">
+                        <li class="nav-item ">
+                          <a class="nav-link tabss" id="vehicle-tab" data-toggle="tab" href="#vehicle" role="tab" aria-controls="vehicle" aria-selected="true">By Vehicle</a>
+                          </li>
+                        <li class="nav-item ">
+                          <a class="nav-link tabss" id="rim-tab" data-toggle="tab" href="#rim" role="tab" aria-controls="rim" aria-selected="true">By Inch</a>
+                          </li>
+                        </ul>
+                      <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="vehicle" role="tabpanel" aria-labelledby="vehicle">
+                          <form action="product.php" method="post">
+                            <div class="container pt-4 pb-4 ">
+                             <div class="d-inline-block col-md-3">
+                                <p>CAR BRANDS</p>
+                                  <select id="typeCar" class="maintain" name="carType"  onchange="TypeFunc();">
+                                    <option value="1">Audi</option>
+                                    <option value="2">BMW</option>
+                                    <option value="3">Ford</option>
+                                    <option value="4">Honda</option>
+                                  </select>
+                              </div>
+                              <div class="d-inline-block col-md-3">
+                                <p>CAR MODEL</p>
+                                <select id="modelCar" class="maintain" name="carModel">
+
 <div id="search-bar" class="row search justify-content-center">
   <div id="title">
     <h5>Find Your Type Here</h5>
@@ -83,22 +115,18 @@ $sort = $sort_handle->sortKeywords($arr, 2);
               </div>
             </div>
 
-        </div>
-      </div>
-      <div class="dash">
-        <h5>|</h5>
-      </div>
-      <div class="size">
-        <div class="dropdown">
-          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            By Size
-          </button>
-          <div id="size-wrapper" class="dropdown-menu">
-              <div class="dropdown-item item-wrapper">
-                <div class="row col-md-12">
-                     <div class="col-md-6 col-12">
-                         <form action="product.php" method="post">
-                             <div class="col-md-6">
+                                </select>
+                              </div>
+                              <div class="col-md-6 submit">
+                                <button type="submit">Submit</button>
+                              </div>
+                            </div>
+                          </form>
+                          </div>
+                          <div class="tab-pane fade " id="rim" role="tabpanel" aria-labelledby="rim-tab">
+                          <form action="product.php" method="post">
+                            <div class="container pt-4 pb-4 ">
+                             <div class="d-inline-block col-md-3">
                                  <p>By Brand</p>
                                   <select class="maintain" name="rimType">
                                       <option value="1">Audi</option>
@@ -107,9 +135,9 @@ $sort = $sort_handle->sortKeywords($arr, 2);
                                       <option value="4">Honda</option>
                                   </select>
                               </div>
-                              <div class="col-md-6">
+                              <div class="d-inline-block col-md-3">
                                  <p>By Inch</p>
-                                  <select id="inch" class="maintain rol-md-5 rol-xs-12" name="size">
+                                  <select id="inch" class="maintain col-md-5 col-xs-12" name="size">
                                       <!-- <option value="13">13</option>
                                       <option value="14">14</option>
                                       <option value="15">15</option>
@@ -123,18 +151,24 @@ $sort = $sort_handle->sortKeywords($arr, 2);
                               <div class="col-md-6 submit">
                                  <button type="submit">Submit</button>
                               </div>
-                            </form>
-                             </div>
-                            <div class="col-md-6 col-12" >
-                                  <img src="img/icon-tyre.png">
-                            </div>
+                          </div>
+                        </form>
                         </div>
                       </div>
                     </div>
+                  </div>
                 </div>
-            </div>
-        </div>
-</div>
+              </div>
+
+
+
+
+
+  </div>
+
+
+
+
 
   <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
 
@@ -169,34 +203,25 @@ $sort = $sort_handle->sortKeywords($arr, 2);
       <span class="sr-only">Next</span>
     </a>
   </div>
-<div class ="container-fluid col-12 pt-4 pb-4 tabcolor">
-<ul class="nav nav-tabs " id="myTab" role="tablist">
-  <li class="nav-item ">
-    <a class="nav-link tabss" id="vehicle-tab" data-toggle="tab" href="#vehicle" role="tab" aria-controls="vehicle" aria-selected="true">By Vehicle</a>
-  </li>
-  <li class="nav-item ">
-    <a class="nav-link tabss" id="rim-tab" data-toggle="tab" href="#rim" role="tab" aria-controls="rim" aria-selected="true">By Inch</a>
-  </li>
-</ul>
 
 
 <div class="tab-content" id="myTabContent">
   <div class="tab-pane fade show active" id="vehicle" role="tabpanel" aria-labelledby="vehicle">
   <form action="#" method="post">
     <div class="container pt-4 pb-4 ">
-         <div class="d-inline-block col-md-3">
-            <p>CAR BRANDS</p>
-              <select id="typeCar" class="maintain" name="carType"  onchange="TypeFunc();">
-                  <!-- 车款选项 -->
-              </select>
-          </div>
-          <div class="d-inline-block col-md-3">
-             <p>CAR MODEL</p>
-              <select id="modelCar" class="maintain" name="carModel">
-                  <!-- 车形选项 -->
-                  <option>Car Model</option>
-              </select>
-          </div>
+                                     <div class="d-inline-block col-md-3">
+                                        <p>CAR BRANDS</p>
+                                          <select id="typeCar" class="maintain" name="carType"  onchange="TypeFunc();">
+                                              <!-- 车款选项 -->
+                                          </select>
+                                      </div>
+                                      <div class="d-inline-block col-md-3">
+                                         <p>CAR MODEL</p>
+                                          <select id="modelCar" class="maintain" name="carModel">
+                                              <!-- 车形选项 -->
+                                              <option>Car Model</option>
+                                          </select>
+                                      </div>
       </div>
     </form>
 
@@ -223,33 +248,33 @@ $sort = $sort_handle->sortKeywords($arr, 2);
   <div class="tab-pane fade " id="rim" role="tabpanel" aria-labelledby="rim-tab">
   <form action="product.php" method="post">
   <div class="container pt-4 pb-4 ">
-     <div class="d-inline-block col-md-3">
-         <p>By Brand</p>
-          <select id="typeModel" class="maintain" name="rimType">
-               <!-- 轮框第一参数 -->
-          </select>
+                             <div class="d-inline-block col-md-3">
+                                 <p>By Brand</p>
+                                  <select id="typeModel" class="maintain" name="rimType">
+                                       <!-- 轮框第一参数 -->
+                                  </select>
+                              </div>
+                              <div class="d-inline-block col-md-3">
+                                 <p>By Inch</p>
+                                  <select id="rimWitdh" class="maintain col-md-5 col-xs-12" name="size">
+                                       <!-- 轮框第二参数 -->
+                                       <option>Rim Width</option>
+                                  </select>
+                              </div>
+                              <div class="d-inline-block col-md-3">
+                                 <p>By Inch</p>
+                                  <select id="rimWitdh" class="maintain col-md-5 col-xs-12" name="size">
+                                       <!-- 轮框第三参数 -->
+                                  </select>
+                              </div>
+                              <div class="d-inline-block col-md-3">
+                                 <p>By Inch</p>
+                                  <select id="rimWitdh" class="maintain col-md-5 col-xs-12" name="size">
+                                       <!-- 轮框第四参数 -->
+                                  </select>
+                              </div>
       </div>
-      <div class="d-inline-block col-md-3">
-         <p>By Inch</p>
-          <select id="rimWitdh" class="maintain col-md-5 col-xs-12" name="size">
-               <!-- 轮框第二参数 -->
-               <option>Rim Width</option>
-          </select>
-      </div>
-      <div class="d-inline-block col-md-3">
-         <p>By Inch</p>
-          <select id="rimWitdh" class="maintain col-md-5 col-xs-12" name="size">
-               <!-- 轮框第三参数 -->
-          </select>
-      </div>
-      <div class="d-inline-block col-md-3">
-         <p>By Inch</p>
-          <select id="rimWitdh" class="maintain col-md-5 col-xs-12" name="size">
-               <!-- 轮框第四参数 -->
-          </select>
-      </div>
-  </div>
-  </form>
+    </form>
     <div>
         <table id="rimTable">
          <img id="imgTitle" src="img/logo/WW.png" style="height:50px; width:50px;">
