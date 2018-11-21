a9793cdc9023235f82768ffa694a87a2ebe57eaa
Liew Yee James <james@Liews-MBP.lan>
2018-11-22 01:12:26 +0800

    首頁呈現方式，文字標題的輪播圖

--- index.php
+++ index.php
@@ -30,8 +30,11 @@ $sort = $sort_handle->sortKeywords($arr, 2);
   <link href="css/header.css" rel="stylesheet">
   <link href="css/footer.css" rel="stylesheet">
   <script src="js/jquery.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
   <script src="js/index.js"></script>
   <script src="js/header.js"></script>
+  <script src="js/slider.js"></script>
+
 </head>
 <body onscroll="backTop()">
 <?php include('header.html') ?>
@@ -98,40 +101,106 @@ $sort = $sort_handle->sortKeywords($arr, 2);
   </div>
 </div>
 
-  <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
-
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <!-- <li data-target="#carouselExampleIndicators" data-slide-to="2"></li> -->
-    </ol>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img class="d-block w-100 " src="img/6.jpg" alt="First slide">
-        <div class="carousel-caption d-none d-md-block">
-          <!-- <h1>Product</h1>
-          <img src= -->
+<main class="main-content">
+    <section class="slideshow">
+        <div class="slideshow-inner">
+            <div class="slides">
+                <div class="slide is-active ">
+                    <div class="slide-content">
+                        <div class="caption">
+                            <div class="title">Slide title 1</div>
+                            <div class="text">
+                                <p>Slide description 1</p>
+                            </div>
+                            <a href="#" class="button">
+                                <span class="btn-inner">Learn More</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="image-container">
+                        <img src="img/1.jpg" alt="" class="image" />
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="slide-content">
+                        <div class="caption">
+                            <div class="title">Slide title 2</div>
+                            <div class="text">
+                                <p>Slide description 2</p>
+                            </div>
+                            <a href="#" class="button">
+                                <span class="btn-inner">Learn More</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="image-container">
+                        <img src="img/2.jpg" alt="" class="image" />
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="slide-content">
+                        <div class="caption">
+                            <div class="title">Slide title 3</div>
+                            <div class="text">
+                                <p>Slide description 3</p>
+                            </div>
+                            <a href="#" class="button">
+                                <span class="btn-inner">Learn More</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="image-container">
+                        <img src="img/3.jpg" alt="" class="image" />
+                    </div>
+                </div>
+                <div class="slide">
+                    <div class="slide-content">
+                        <div class="caption">
+                            <div class="title">Slide title 4</div>
+                            <div class="text">
+                                <p>Slide description 4</p>
+                            </div>
+                            <a href="#" class="button">
+                                <span class="btn-inner">Learn More</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="image-container">
+                        <img src="img/4.jpg" alt="" class="image" />
+                    </div>
+                </div>
+            </div>
+            <div class="pagination">
+                <div class="item is-active">
+                    <span class="icon">1</span>
+                </div>
+                <div class="item">
+                    <span class="icon">2</span>
+                </div>
+                <div class="item">
+                    <span class="icon">3</span>
+                </div>
+                <div class="item">
+                    <span class="icon">4</span>
+                </div>
+            </div>
+            <div class="arrows">
+                <div class="arrow prev">
+          <span class="svg svg-arrow-left">
+            <svg version="1.1" id="svg4-Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="14px" height="26px" viewBox="0 0 14 26" enable-background="new 0 0 14 26" xml:space="preserve"> <path d="M13,26c-0.256,0-0.512-0.098-0.707-0.293l-12-12c-0.391-0.391-0.391-1.023,0-1.414l12-12c0.391-0.391,1.023-0.391,1.414,0s0.391,1.023,0,1.414L2.414,13l11.293,11.293c0.391,0.391,0.391,1.023,0,1.414C13.512,25.902,13.256,26,13,26z"/> </svg>
+            <span class="alt sr-only"></span>
+          </span>
+                </div>
+                <div class="arrow next">
+          <span class="svg svg-arrow-right">
+            <svg version="1.1" id="svg5-Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="14px" height="26px" viewBox="0 0 14 26" enable-background="new 0 0 14 26" xml:space="preserve"> <path d="M1,0c0.256,0,0.512,0.098,0.707,0.293l12,12c0.391,0.391,0.391,1.023,0,1.414l-12,12c-0.391,0.391-1.023,0.391-1.414,0s-0.391-1.023,0-1.414L11.586,13L0.293,1.707c-0.391-0.391-0.391-1.023,0-1.414C0.488,0.098,0.744,0,1,0z"/> </svg>
+            <span class="alt sr-only"></span>
+          </span>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100 " src="img/5.jpg"  alt="Second slide">
-      </div>
-
-      <div class="carousel-item">
-        <img class="d-block w-100" src="img/4.jpg" alt="Second slide">
-      </div>
-
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>  
+    </section>
+</main>
 
   
 <!-- linked main.css-->
